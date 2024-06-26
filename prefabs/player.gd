16d776b9a3c73fa29797612b69e3b356ca576b97
JoseCarlitosGordo <gorjc003@mymail.unisa.edu.au>
2024-06-26 14:10:03 +0930

    please do not crash

--- prefabs/player.gd
+++ prefabs/player.gd
@@ -23,6 +23,7 @@ func _physics_process(delta):
 	var direction = Input.get_axis("move_left", "move_right")
 	if direction:
 		velocity.x = direction * SPEED
+		$AnimatedSprite2D.play("walk")
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
