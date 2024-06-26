a06ba1ac0319f2367106dac6fe9614aabb7204d1
sirsegv <squidinkrf@gmail.com>
2024-06-26 18:59:11 +0930

    finish game

--- prefabs/goal.gd
+++ prefabs/goal.gd
@@ -1,10 +1,14 @@
 extends Area2D
 
+var opened = false
+
 func _ready():
 	$Sprite.play("default")
 
 func _on_body_entered(body: Node2D) -> void:
-	if body.name == "Player":
+	if body.name == "Player" and not opened:
+		opened = true
+		
 		$Sprite.play("open")
 		%UI.win_state()
 		await get_tree().create_timer(0.5).timeout
